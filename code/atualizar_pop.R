a506a78197d4d3cb8d282ad8c163cf2567303beb
Gabriel <gabrielzanlorenssi@gmail.com>
2020-11-25 11:40:56 -0300

    lista de partidos

--- code/atualizar_pop.R
+++ code/atualizar_pop.R
@@ -22,3 +22,17 @@ popMunic2 %>%
   bind_rows(pop2020) -> popMunic
 
 usethis::use_data(popMunic, overwrite = TRUE)
+
+
+#-- info partidos
+infoPartidos %>%
+  set_names('party', 'abbrev2020', 'abbrev2016', 'number', 'hex') %>%
+  drop_na(number) %>%
+  add_row(party="Partido Pátria Livre",
+          abbrev2020 = NA,
+          abbrev2016 = "PPL",
+          number = 54,
+          hex = "#CB8C91") %>%
+  mutate(active = ifelse(number %in% c(31,54,44), FALSE, TRUE)) -> infoParty
+
+usethis::use_data(infoParty, overwrite = TRUE)
